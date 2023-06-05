6de6ab2ce67ffa44b210eca69f79de0285323a22
ggghosthat <ildarildar990@gmail.com>
2023-06-05 16:36:20 +0300

    add dashbord; update functionality for not and task objects

--- podo/podo/main.py
+++ podo/podo/main.py
@@ -1,21 +1,59 @@
 from datetime import datetime
 from models.types import Note, Task
-from base import session, Base, engine
+from base import session
 
+class Dashboard:
+    pool = session.query(Note).order_by(Note.created.desc())
+    pool_count = len(pool)
+    step = 10
+    position = 0
 
-# note1 = Note(title="Hello World", created=datetime.now())
-# Base.metadata.create_all(bind=engine)
-# session.add(note1)
-# session.commit()
+    def get_single(self, id):
+        try:
+            note = self.pool.filter(Note.nid==id).first()
+            return note
+        except Exception as ex :
+            # TODO rise exception within newly implemented logger
+            pass
 
-# note = session.query(Note).first()
-# note.add_task(raw="wake up", timeset=datetime.now())
+    def get_notes(self):
+        try:
+            if self.position > self.pool_count:
+                return
+            notes = self.pool.limit(self.step).offset(self.position)
+            self.position = self.position + self.step
+            return notes
+        except Exception as ex :
+            # TODO rise exception within newly implemented logger
+            pass
+    
+    def add_note(self, 
+                 new_title,
+                 new_description):
+        try:
+            note = Note(title=new_title,
+                        description=new_description, 
+                        created=datetime.now())
+            session.add(note)
+            session.commit()
+        except Exception as ex :
+            # TODO rise exception within newly implemented logger
+            pass
 
-notes = session.query(Task).all()
+    def update_note(self,
+                    id,
+                    new_title = None,
+                    new_description = None):        
+        try:
+            note = session.query(Note).filter(Note.nid==id).first()
 
-print("--------------")
-for task in notes:
-    print(task.raw)
-    print(task.note.title)
+            if new_title != None:
+                note.title = new_title
+            if new_description != None:
+                note.new_description = new_description
 
-print(len(notes))
+            if (new_title != None) or (new_description != None):
+                session.commit()
+        except Exception as ex :
+            # TODO rise exception within newly implemented logger
+            pass
--- podo/podo/models/types.py
+++ podo/podo/models/types.py
@@ -8,16 +8,57 @@ class Note(Base):
 
     nid = Column("nid", Integer, primary_key=True, autoincrement=True, nullable=False)
     title = Column("title", String)
+    description = Column("description", String)
     created = Column("created", DateTime)
+    checked = Column("checked", Boolean, default=False)
 
     def add_task(self, raw, timeset):
-        task = Task(raw=raw, 
-                    created=datetime.now(),
-                    timeset=timeset,
-                    note_id=self.nid)
+        try:
+            task = Task(raw=raw, 
+                        created=datetime.now(),
+                        timeset=timeset,
+                        note_id=self.nid)
+            
+            session.add(task)
+            session.commit()
+        except Exception as ex :
+            # TODO rise exception within newly implemented logger
+            pass    
+
+    def remove_task(self, tid):
+        try:
+            session.query(Task).filter(Task.tid==tid).delete()
+            session.commit()
+        except Exception as ex :
+            # TODO rise exception within newly implemented logger
+            pass
+
+    def update_task(self, 
+                    task_id,
+                    new_raw = None, 
+                    new_note = None, 
+                    new_priority = None):
+        try:
+            task = session.query(Task).filter(Task.tid==task_id).first()
+            
+            if new_raw != None:
+                task.raw = new_raw
+            if new_note != None:
+                task.note_id = new_note
+            if new_priority != None:
+                task.priority = new_priority
+
+            if (new_raw != None) or (new_note != None) or (new_priority != None):
+                session.commit()
+        except Exception as ex :
+            # TODO rise exception within newly implemented logger
+            pass
         
-        session.add(task)
-        session.commit()
+    def check(self):
+        if self.checked == False:
+            self.checked = True
+        else:
+            self.checked = False
 
 class Task(Base):
     __tablename__ = 'tasks'
@@ -28,6 +69,7 @@ class Task(Base):
     timeset = Column("timeset", DateTime)
     isCompleted = Column("is-completed", Boolean)
     completed = Column("completed", DateTime)
+    priority = Column("priority", Integer)
 
-    note_id = Column(Integer, ForeignKey("notes.nid"))
+    note_id = Column(Integer, ForeignKey("notes.nid"), nullable=True)
     note = relationship("Note", foreign_keys=note_id)
